b099c6a028df2a04ea753684cf96c1bb9d18e346
Lugra <lugra.dev@gmail.com>
2026-02-18 00:08:26 +0100

    Configurar entrada desktop y visor de logs en tiempo real con el servidor

---
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -0,0 +1,21 @@
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.WindowPosition
+
+fun main() = application {
+    val mainWindowState = rememberWindowState(
+        width = 800.dp,
+        height = 900.dp,
+        position = WindowPosition(x = 50.dp, y = 50.dp)
+    )
+
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "Wordle Multiplatform",
+        state = mainWindowState
+    ) {
+        App()
+    }
+}
